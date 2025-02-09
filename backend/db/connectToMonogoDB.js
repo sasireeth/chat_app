3ef190e6921c00c484315c475e6e6371d53ef211
sasireeth <108484893+sasireeth@users.noreply.github.com>
2025-02-09 17:23:43 +0530

    Update connectToMonogoDB.js

--- backend/db/connectToMonogoDB.js
+++ backend/db/connectToMonogoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB= async() =>{
     try {
-        await mongoose.connect(mongodb+srv://sasireethreddy:uIgY2q4Ga81YAd4Y@cluster0.enpbjj5.mongodb.net/chat-app-db?);
+        await mongoose.connect(mongodb+srv://sasireethreddy:uIgY2q4Ga81YAd4Y@cluster0.enpbjj5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0);
         console.log("Connected to MongoDB")
     } catch (error) {
         console.log("Error connecting to MongoDB",error.message);
